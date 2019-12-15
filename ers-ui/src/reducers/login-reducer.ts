0f9d82f9e9a44338b4a9d558ee5fb5162c4943a1
mahmud <mahmudskarim@gmail.com>
2019-12-15 16:37:05 -0500

    Reducer fixed

--- ers-ui/src/reducers/login-reducer.ts
+++ ers-ui/src/reducers/login-reducer.ts
@@ -1,6 +1,7 @@
 import { ILoginState } from ".";
 import { User } from "../models/user";
 import { Role } from "../models/role";
+import { ersLoginType } from "../action-mappers/login-action-mappers";
 
 const initialState: ILoginState = {
     user: new User(0, '', '', '', '', '', new Role(0, ''))
@@ -8,6 +9,12 @@ const initialState: ILoginState = {
 
 export const loginReducer = (state = initialState, action: any) => {
     switch (action.type) {
+        case ersLoginType.SUCCESSFUL_LOGIN: {
+            return {
+                ...state,
+                user: action.payload.user
+            }
+        }
         default:
             return state
     }
