064446c7e08a7f2e7798d40a1f01f9297c6e4170
mahmud <mahmudskarim@gmail.com>
2019-12-15 16:07:58 -0500

    fixed errors

--- ers-ui/src/App.tsx
+++ ers-ui/src/App.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import Box from '@material-ui/core/Box';
 import './App.css';
-import { Container, Typography, Button } from '@material-ui/core';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import LoginComponent from './components/login-component/LoginContainer';
 import { Provider } from 'react-redux';
 import { store } from './Store';
@@ -14,16 +12,9 @@ const App: React.FC = () => {
         <Router>
           <Switch>
             <Route path='/login' component={LoginComponent} />
-            <Container maxWidth="sm">
-              <Box my={4}>
-                <Typography variant="h4" component="h1" gutterBottom>
-                  Breaking ERS Bad!
-                </Typography>
-                <Link to="/login">
-                  <Button variant="contained" color="secondary">Enter if you Dare</Button>
-                </Link>
-              </Box>
-            </Container>
+            <Route path='/'>
+              <LoginComponent />
+            </Route>
           </Switch>
         </Router>
       </Provider>
